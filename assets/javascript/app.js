32c080cd2af132a3ec094c30dedb24dac7fda6e2
Isaac Kwon <you@example.com>
2017-10-03 18:30:26 -0700

    Basic Functionality Done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,21 @@
 let score = 1;
 let timer = 10;
 
-const config = {
-  apiKey: "AIzaSyBzu6I3WrIA1qfrItL2TAc8ilJYEq4wbw8",
-  authDomain: "clickgame-1baff.firebaseapp.com",
-  databaseURL: "https://clickgame-1baff.firebaseio.com",
-  projectId: "clickgame-1baff",
-  storageBucket: "clickgame-1baff.appspot.com",
-  messagingSenderId: "694602255414"
-};
-firebase.initializeApp(config);
+function playGame() {
+  let startTimer = setInterval(() => {
+    $("#timeCount").text("Time Remaining: " + timer);
+    $("#progressBar").val(10 - --timer);
 
-const database = firebase.database();
+    if (timer <= 0) {
+      clearInterval(startTimer);
+      $("#replayButton").prop("disabled", false);
+      $("#button").prop("disabled", true);
+      $("#start").fadeOut();
+      $("#replayButton").fadeIn();
+      $("#highScore").text(`Your Score: ${score - 1}`);
+    }
+  }, 1000);
+}
 
 $("#replayButton").hide();
 
@@ -22,41 +26,13 @@ $("#button").on("click", () => {
 
 $("#start").on("click", () => {
   $("#start").prop("disabled", true);
-
-  function playGame() {
-    let startTimer = setInterval(() => {
-      $("#timeCount").text("Time Remaining: " + timer);
-      $("#progressBar").val(10 - --timer);
-
-      if (timer <= 0) {
-        clearInterval(startTimer);
-        $("#replayButton").prop("disabled", false);
-        $("#button").prop("disabled", true);
-        $("#start").fadeOut();
-        $("#replayButton").fadeIn();
-        const highScore = {
-          score: score - 1
-        };
-        database.ref().push(highScore);
-        database.ref().on("child_added", (childSnapshot) => {
-          const childObj = childSnapshot.val();
-          const childKey = childSnapshot.key;
-
-          //get highest score from the database and display it in html
-          
-        }, (errorObject) => {
-          console.log(errorObject);
-        });
-      }
-    }, 1000);
-  }
   playGame();
+});
 
-  $("#replayButton").on("click", () => {
-    timer = 10;
-    score = 1;
-    $("#replayButton").prop("disabled", true);
-    $("#button").prop("disabled", false);
-    playGame();
-  });
+$("#replayButton").on("click", () => {
+  timer = 10;
+  score = 1;
+  $("#replayButton").prop("disabled", true);
+  $("#button").prop("disabled", false);
+  playGame();
 });
